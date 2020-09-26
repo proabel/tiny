8405e25377bc9680b0fb2a7dc9c8c70461e5ee0a
Abel Dharmaraj <abel@winterfell.domain.name>
2020-09-26 12:01:57 +0530

    removed the other function

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const axios = require('axios').default;
 
-module.exports = function getMockData(){
-    return console.log('working mock data');
-}
-
 module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
