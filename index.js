df4df7cdcd72c3520ef813c23426ed3ff279b5f5
Abel Dharmaraj <abel@winterfell.local>
2020-09-30 10:48:21 +0530

    added mock data

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
+import axios from 'axios';
+
+function getMockData(){
+    axios.get("https://tinycall.free.beeceptor.com").then(res => {
+        console.log(res);
+    }).catch(e => {
+        console.error(e);
+    })
+}
+
 function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
 };
 
 module.exports.tiny = tiny;
+module.exports.getMockData = getMockData;
